bc36d1a00107709f43e60cb1e9dda68287223520
Francois Bastardie <fba@aqua.dtu.dk>
2017-06-13 17:53:27 +0200

    Set to 99 vessels by duplication

--- vesselsspe_fake/DuplicatingVessels.R
+++ vesselsspe_fake/DuplicatingVessels.R
@@ -2,8 +2,8 @@
 
 # duplicate files from a vessel
 
-namefiles <-          c( "_gshape_cpue_per_stk_on_nodes_quarter", "_freq_possible_metiers_quarter", "_gscale_cpue_per_stk_on_nodes_quarter", "_gshape_cpue_per_stk_on_nodes_quarter",
-                             "_possible_metiers_quarter")
+namefiles <-          c( "_gshape_cpue_per_stk_on_nodes_quarter", "_freq_possible_metiers_quarter", "_gscale_cpue_per_stk_on_nodes_quarter",
+                             "_possible_metiers_quarter", "_cpue_per_stk_on_nodes_quarter")
 
 
 for (namefile in namefiles) {
@@ -11,8 +11,8 @@ for (namefile in namefiles) {
   for (Q in 1:4){
     dd <- read.table(paste("C:\\Users\\fbas\\Documents\\GitHub\\DISPLACE_input_test2\\vesselsspe_fake\\DNK007",namefile, Q, ".dat", sep=""), header=TRUE)
 
- #    for (vesselid in 8:9) write.table(dd, file=paste("C:\\Users\\fbas\\Documents\\GitHub\\DISPLACE_input_test2\\vesselsspe_fake\\DNK00",vesselid,namefile,Q,".dat", sep=""),col.names=TRUE, row.names=FALSE, sep=" ", quote=FALSE)
- #    for (vesselid in 10:30) write.table(dd, file=paste("C:\\Users\\fbas\\Documents\\GitHub\\DISPLACE_input_test2\\vesselsspe_fake\\DNK0",vesselid,namefile,Q,".dat", sep=""),col.names=TRUE, row.names=FALSE, sep=" ", quote=FALSE)
+     for (vesselid in 8:9) write.table(dd, file=paste("C:\\Users\\fbas\\Documents\\GitHub\\DISPLACE_input_test2\\vesselsspe_fake\\DNK00",vesselid,namefile,Q,".dat", sep=""),col.names=TRUE, row.names=FALSE, sep=" ", quote=FALSE)
+    for (vesselid in 10:30) write.table(dd, file=paste("C:\\Users\\fbas\\Documents\\GitHub\\DISPLACE_input_test2\\vesselsspe_fake\\DNK0",vesselid,namefile,Q,".dat", sep=""),col.names=TRUE, row.names=FALSE, sep=" ", quote=FALSE)
      for (vesselid in 31:99) write.table(dd, file=paste("C:\\Users\\fbas\\Documents\\GitHub\\DISPLACE_input_test2\\vesselsspe_fake\\DNK0",vesselid,namefile,Q,".dat", sep=""),col.names=TRUE, row.names=FALSE, sep=" ", quote=FALSE)
      }
    }
@@ -23,11 +23,11 @@ for (namefile in namefiles) {
    for (namefile in c("vesselsspe_betas_semester", "vesselsspe_percent_tacs_per_pop_semester")){
   for (S in 1:2){
    dd <- read.table(paste("C:\\Users\\fbas\\Documents\\GitHub\\DISPLACE_input_test2\\vesselsspe_fake\\",namefile,S,".dat", sep=""), header=TRUE)
-    for (vesselid in 8:9){
-      obj <- dd[dd[,1]=="DNK007", ]
-      obj[,1] <- paste("DNK00",vesselid, sep='') 
-      dd <- rbind.data.frame (dd, obj)
-      }
+  #  for (vesselid in 8:9){
+  #    obj <- dd[dd[,1]=="DNK007", ]
+  #    obj[,1] <- paste("DNK00",vesselid, sep='') 
+  #    dd <- rbind.data.frame (dd, obj)
+  #    }
     for (vesselid in 10:99){
       obj <- dd[dd[,1]=="DNK007", ]
       obj[,1] <- paste("DNK0",vesselid, sep='') 
@@ -43,11 +43,11 @@ for (namefile in namefiles) {
      for (Q in 1:4){
       dd <- read.table(paste("C:\\Users\\fbas\\Documents\\GitHub\\DISPLACE_input_test2\\vesselsspe_fake\\",namefile,Q,".dat", sep=""), header=TRUE)
      
-      for (vesselid in 8:9){
-         obj <- dd[dd[,1]=="DNK007", ]
-         obj[,1] <- paste("DNK00",vesselid, sep='') 
-         dd <- rbind.data.frame (dd, obj)
-         }
+    #  for (vesselid in 8:9){
+    #     obj <- dd[dd[,1]=="DNK007", ]
+    #     obj[,1] <- paste("DNK00",vesselid, sep='') 
+    #     dd <- rbind.data.frame (dd, obj)
+    #     }
       for (vesselid in 31:99){
          obj <- dd[dd[,1]=="DNK007", ]
          obj[,1] <- paste("DNK0",vesselid, sep='') 
@@ -61,17 +61,17 @@ for (namefile in namefiles) {
   
      for (Q in 1:4){
       dd <- read.table(paste("C:\\Users\\fbas\\Documents\\GitHub\\DISPLACE_input_test2\\vesselsspe_fake\\","vesselsspe_features_quarter",Q,".dat", sep=""),  sep="|", header=FALSE)
-      for (vesselid in 8:9){
-         obj <- dd[dd[,1]=="DNK007", ]
-         obj[,1] <- paste("DNK00",vesselid, sep='') 
-         dd <- rbind.data.frame (dd, obj)
-         }     
+     # for (vesselid in 8:9){
+     #    obj <- dd[dd[,1]=="DNK007", ]
+     #    obj[,1] <- paste("DNK00",vesselid, sep='') 
+     #    dd <- rbind.data.frame (dd, obj)
+     #    }     
       for (vesselid in 31:99){
          obj <- dd[dd[,1]=="DNK007", ]
          obj[,1] <- paste("DNK0",vesselid, sep='') 
          dd <- rbind.data.frame (dd, obj)
          }     
-      write.table(dd, file=paste("C:\\Users\\fbas\\Documents\\GitHub\\DISPLACE_input_test2\\vesselsspe_fake\\","vesselsspe_features_quarter",Q,".dat", sep=""), sep="|", col.names=TRUE, row.names=FALSE, sep=" ", quote=FALSE)
+      write.table(dd, file=paste("C:\\Users\\fbas\\Documents\\GitHub\\DISPLACE_input_test2\\vesselsspe_fake\\","vesselsspe_features_quarter",Q,".dat", sep=""), sep="|", col.names=FALSE, row.names=FALSE,  quote=FALSE)
      } 
      
    
